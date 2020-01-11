97aee660be266408b0667ae89094b127101c5630
Justin Graber <justin.graber@dunami.com>
2020-01-11 17:03:59 -0600

    latest wip

--- operators/type.go
+++ operators/type.go
@@ -1,4 +1,4 @@
-package operator
+package operators
 
 type Type interface {
 	Shit()
--- stream/transform/transform_state.go
+++ stream/transform/transform_state.go
@@ -1,4 +1,23 @@
 package transform
 
+import (
+	"container/list"
+	"sync"
+)
+
 type TransformState struct {
+	buffer         *list.List
+	highWaterMark  int
+	draining       bool
+	writeRequested bool
+	ended          bool
+	destroyed      bool
+	mtx            sync.Mutex
+}
+
+func NewTransformState() *TransformState {
+	return &TransformState{
+		buffer:        list.New(),
+		highWaterMark: 3,
+	}
 }
